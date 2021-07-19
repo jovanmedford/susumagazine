7e9bf0eee618b8944b83a3a4cde32889aeb41b2f
Jovan Medford <jovanmedford@gmail.com>
2021-07-19 14:13:01 -0400

    Feat: Add Layout component with twin macro global stlyles
    The global styles import adds preflight based styles used in tailwind along with some animation classes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import * as React from "react"
 import Seo from "../components/seo"
+import Layout from "../components/Layout"
 
 const IndexPage = () => (
+  <Layout>
     <Seo title="Home" />
+  </Layout>
 )
 
 export default IndexPage
