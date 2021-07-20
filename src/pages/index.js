6ad2cb6a15ff53b46490e9ad4593a2071c7075c3
Jovan Medford <jovanmedford@gmail.com>
2021-07-20 08:56:04 -0400

    Chore: Add dummy text to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Layout from "../components/Layout"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <h1>Hey</h1>
+    <h2>How Are You?</h2>
+    <p>This is some basic text</p>
   </Layout>
 )
 
