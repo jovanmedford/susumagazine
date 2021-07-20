cee1ad5c218e198c029fb0d5292ccaec4d29044b
Jovan Medford <jovanmedford@gmail.com>
2021-07-20 16:09:23 -0400

    Feat: Add mobile header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import * as React from "react"
 import Seo from "../components/seo"
 import Layout from "../components/Layout"
+import Logo from "../images/susu-website-logo.png"
+import tw, { styled } from "twin.macro"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+const Header = styled.div`
+  ${tw`mt-4 mx-8 flex justify-between items-center`}
+`
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hey</h1>
-    <h2>How Are You?</h2>
-    <p>This is some basic text</p>
+    <Header>
+      <img src={Logo} />
+      <FontAwesomeIcon
+        icon={faBars}
+        style={{ width: "2rem", height: "2rem" }}
+      />
+    </Header>
   </Layout>
 )
 
