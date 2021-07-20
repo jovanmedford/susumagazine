8f31a4bc122383f40d7a90da9e3e833deede5cb7
Jovan Medford <jovanmedford@gmail.com>
2021-07-20 00:02:24 -0400

    Feat: Add Global styles from theme to layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,19 @@
 import React from "react"
-import { GlobalStyles } from "twin.macro"
+import { Global } from "@emotion/react"
+import { theme, GlobalStyles } from "twin.macro"
 
 const Layout = ({ children, ...rest }) => (
   <div {...rest}>
     <GlobalStyles />
+    <Global
+      styles={{
+        body: {
+          fontFamily: theme`fontFamily.sans`,
+          color: theme`textColor.base`,
+          backgroundColor: theme`colors.bgColor`,
+        },
+      }}
+    />
     {children}
   </div>
 )
