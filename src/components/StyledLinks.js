c5306a47f43a9c9eefe9d0d3a9d606ca3e31184b
Jovan Medford <jovanmedford@gmail.com>
2021-07-21 16:49:31 -0400

    Feat: update hover styles and add transition to BubbleLinks

--- src/components/StyledLinks.js
+++ src/components/StyledLinks.js
@@ -4,7 +4,9 @@ import { Link } from "gatsby"
 const BubbleLink = styled(Link)(({ isActive }) => [
   tw`text-primary-700 bg-primary-100 py-1 px-6
   md:text-md
-  hover:bg-primary-300
+  transition duration-200 ease-in
+  hover:bg-primary-500
+  hover:text-white
   focus-within:outline-accent 
   rounded-lg
   whitespace-nowrap
