0094e2485e2d085b25ca361854b25013980598f5
Jovan Medford <jovanmedford@gmail.com>
2021-07-21 21:04:32 -0400

    Chore: Extract main navigation links to separate data file

---
+++ src/data/mainNavLinks.js
@@ -0,0 +1,7 @@
+const mainNavigationLinks = [
+  { to: "#", text: "Magazine Library" },
+  { to: "#", text: "Write With Us" },
+  { to: "#", text: "Contact" },
+]
+
+export default mainNavigationLinks
