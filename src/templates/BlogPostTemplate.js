02b6508d5ba46117b6b5a6daaf02d9c5d9bcaacc
Jovan Medford <jovanmedford@gmail.com>
2021-07-24 16:48:13 -0400

    Fix: Correct responsive elemets and styling for blog post template

--- src/templates/BlogPostTemplate.js
+++ src/templates/BlogPostTemplate.js
@@ -5,19 +5,37 @@ import tw, { css } from "twin.macro"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const blogPostStyles = css`
+  ${tw`mt-5 mx-auto lg:mt-10`}
   width: 90%;
-  ${tw`mt-5 max-w-sm mx-auto`}
+
+  max-width: 42ch;
+
+  @media screen and (min-width: 768px) {
+    max-width: 48ch;
+  }
+
+  @media screen and (min-width: 1024px) {
+    max-width: 56ch;
+  }
 
   p {
-    ${tw`font-serif leading-normal mb-4`}
+    ${tw`font-serif leading-normal mb-4
+         lg:text-md lg:mb-6`}
   }
 
   h2 {
-    ${tw`mt-4 mb-2 font-medium text-lg`}
+    ${tw`mt-8 mb-2 font-bold text-primary-500 text-lg 
+        lg:text-xl lg:mt-12
+        `}
   }
 
   h3 {
-    ${tw`mt-2 mb-1 font-medium text-md`}
+    ${tw`mt-6 mb-4 font-bold text-md 
+        lg:text-lg lg:mt-10 lg:mb-6`}
+  }
+
+  a {
+    ${tw`text-primary-700 underline`}
   }
 
   strong {
@@ -29,7 +47,7 @@ function retieveImage(data) {
   let image = ""
   try {
     image = getImage(data.wpPost.featuredImage.node.localFile)
-  } catch(e) {
+  } catch (e) {
     console.log(e)
   }
   return image
@@ -46,19 +64,21 @@ const BlogPostTemplate = ({ data }) => {
 
   return (
     <Layout>
-      <section tw='mx-auto md:flex'>
-      <div tw="ml-8 md:max-w-xs">
-        <span tw="block mt-4 text-primary-700 text-lg">Personal Finance</span>
-        <h1 tw="text-lg">{title}</h1>
-        <div tw="flex mt-2">
-          <img tw="w-14 h-14 mr-2 rounded-full" src={authorImg}/>
-          <div>
-            <span tw='block'>Last Modified: {date}</span>
-            <span tw='block'>By {firstName} {lastName}</span>
+      <section tw="mt-8 mx-auto md:flex md:w-10/12 max-w-2xl">
+        <div tw="flex flex-col justify-center mx-auto w-11/12 max-w-xs md:w-6/12">
+          <span tw="block mt-4 text-primary-700 text-lg">Personal Finance</span>
+          <h1 tw="text-lg">{title}</h1>
+          <div tw="flex mt-2">
+            <img tw="w-14 h-14 mr-2 rounded-full" src={authorImg} />
+            <div>
+              <span tw="block">Last Modified: {date}</span>
+              <span tw="block">
+                By {firstName} {lastName}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <GatsbyImage tw="mt-4 rounded-lg mx-auto" image={image} />
+        <GatsbyImage tw="mt-4 rounded-lg mx-auto" image={image} />
       </section>
       <article
         css={blogPostStyles}
@@ -91,6 +111,7 @@ export const query = graphql`
               gatsbyImageData(
                 layout: FIXED
                 width: 352
+                height: 198
                 placeholder: BLURRED
                 formats: [AUTO, WEBP, AVIF]
               )
