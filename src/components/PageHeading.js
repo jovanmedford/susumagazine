bb240d68eaf64eebc6bffee8d10d314360755931
Jovan Medford <jovanmedford@gmail.com>
2021-07-29 10:46:40 -0400

    Feat: Add page heading component
    Add styled component for h1 tag

---
+++ src/components/PageHeading.js
@@ -0,0 +1,8 @@
+import tw, { styled } from "twin.macro"
+
+const PageHeading = styled("h1")`
+  ${tw`font-bold text-primary-700 text-lg text-center mt-8
+       md:text-xl md:mt-12`}
+`
+
+export default PageHeading
