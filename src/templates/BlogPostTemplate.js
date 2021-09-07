83d22fe69b9a5bb3f5c4e76a57f4470871835546
Jovan Medford <jovanmedford@gmail.com>
2021-09-07 17:10:19 -0400

    Feat: Add author Bio

--- src/templates/BlogPostTemplate.js
+++ src/templates/BlogPostTemplate.js
@@ -5,11 +5,57 @@ import tw, { css } from "twin.macro"
 import { GatsbyImage } from "gatsby-plugin-image"
 import retrieveImage from "../utils/retrieveImage"
 
-const blogPostStyles = css`
-  ${tw`mt-5 mx-auto lg:mt-10`}
-  width: 90%;
+const BlogPostTemplate = ({ data }) => {
+  const title = data.wpPost.title
+  const content = data.wpPost.content
+  const image = retrieveImage(data.wpPost)
+  const firstName = data.wpPost.author.node.firstName
+  const lastName = data.wpPost.author.node.lastName
+  const authorImg = data.wpPost.author.node.avatar.url
+  const authorBio = data.wpPost.author.node.description
+  const date = data.wpPost.date
 
+  return (
+    <Layout>
+      <section tw="mt-8 mx-auto md:flex md:w-10/12 max-w-2xl">
+        <div tw="flex flex-col justify-center mx-auto w-11/12 max-w-xs md:w-6/12">
+          <span tw="block mt-4 text-primary-700 text-lg">Personal Finance</span>
+          <h1 tw="text-lg">{title}</h1>
+          <div tw="flex mt-2">
+            <img tw="w-14 h-14 mr-2 rounded-full" src={authorImg} />
+            <div>
+              <span tw="block">Last Modified: {date}</span>
+              <span tw="block">
+                By {firstName} {lastName}
+              </span>
+            </div>
+          </div>
+        </div>
+        <GatsbyImage tw="mt-4 rounded-lg mx-auto" image={image} />
+      </section>
+      <article
+        css={blogPostStyles}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+      <div css={bioStyle}>
+        <hr />
+        <p>{authorBio}</p>
+        <hr />
+      </div>
+    </Layout>
+  )
+}
+
+const containerStyle = css`
+  ${tw`mt-5 mx-auto lg:mt-10`}
+
+  width: 90%;
   max-width: 42ch;
+
+  p {
+    ${tw`font-serif leading-normal mb-4
+         lg:text-md lg:mb-6`}
+  }
 
   @media screen and (min-width: 768px) {
     max-width: 48ch;
@@ -18,11 +64,10 @@ const blogPostStyles = css`
   @media screen and (min-width: 1024px) {
     max-width: 56ch;
   }
+`
 
-  p {
-    ${tw`font-serif leading-normal mb-4
-         lg:text-md lg:mb-6`}
-  }
+const blogPostStyles = css`
+  ${containerStyle}
 
   h2 {
     ${tw`mt-8 mb-2 font-bold text-primary-500 text-lg 
@@ -69,40 +114,18 @@ const blogPostStyles = css`
   }
 `
 
-const BlogPostTemplate = ({ data }) => {
-  const title = data.wpPost.title
-  const content = data.wpPost.content
-  const image = retrieveImage(data.wpPost)
-  const firstName = data.wpPost.author.node.firstName
-  const lastName = data.wpPost.author.node.lastName
-  const authorImg = data.wpPost.author.node.avatar.url
-  const date = data.wpPost.date
+const bioStyle = css`
+  ${containerStyle}
 
-  return (
-    <Layout>
-      <section tw="mt-8 mx-auto md:flex md:w-10/12 max-w-2xl">
-        <div tw="flex flex-col justify-center mx-auto w-11/12 max-w-xs md:w-6/12">
-          <span tw="block mt-4 text-primary-700 text-lg">Personal Finance</span>
-          <h1 tw="text-lg">{title}</h1>
-          <div tw="flex mt-2">
-            <img tw="w-14 h-14 mr-2 rounded-full" src={authorImg} />
-            <div>
-              <span tw="block">Last Modified: {date}</span>
-              <span tw="block">
-                By {firstName} {lastName}
-              </span>
-            </div>
-          </div>
-        </div>
-        <GatsbyImage tw="mt-4 rounded-lg mx-auto" image={image} />
-      </section>
-      <article
-        css={blogPostStyles}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </Layout>
-  )
-}
+  p {
+    ${tw`mt-4 font-bold`}
+  }
+
+  hr {
+    border-top-width: 1.5px;
+    ${tw`border-primary-500`};
+  }
+`
 
 export const query = graphql`
   query ($id: String) {
@@ -115,6 +138,7 @@ export const query = graphql`
         node {
           firstName
           lastName
+          description
           avatar {
             url
           }
