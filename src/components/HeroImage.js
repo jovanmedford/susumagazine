a6917c64cbadbc7ce4e2b63b928e5e9a294e7956
Jovan <jovanmedford@gmail.com>
2025-10-16 20:01:33 -0400

    Feat: add clean to build command, and add missing link

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,19 +1,21 @@
 import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import "twin.macro"
-import { Link } from "gatsby"
 
 export default function HeroImage() {
   return (
-    <Link tw="static col-span-2 transition ease-in-out bg-primary-100 hover:bg-primary-300 mt-6 mb-4 block"  to="susumagazine-2024">
+    <a
+      tw="static col-span-2 transition ease-in-out bg-primary-100 hover:bg-primary-300 mt-6 mb-4 block"
+      href="https://online.fliphtml5.com/bkdcf/ygwe/#p=1"
+    >
       <StaticImage
         tw="w-32 m-auto relative -top-2 -left-3 md:left-5  
           md:m-0 shadow-xl md:shadow-2xl hover:cursor-pointer"
-        src="../images/susumagazine-2024-v3.jpg"
-        alt="2023 Magazine"
+        src="../images/susumagazine-2025.webp"
+        alt="2025 Magazine"
         aspectRatio={0.717}
         layout="fullWidth"
       />
-    </Link>
+    </a>
   )
 }
